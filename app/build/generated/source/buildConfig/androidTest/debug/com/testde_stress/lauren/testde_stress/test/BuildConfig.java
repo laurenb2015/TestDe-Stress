d0482cac68f929492b3949f2e533de2b73900359
Lauren <lauren.berdick@gmail.com>
2015-12-06 17:03:57 +0100

    first commit dec 6

---
+++ app/build/generated/source/buildConfig/androidTest/debug/com/testde_stress/lauren/testde_stress/test/BuildConfig.java
@@ -0,0 +1,13 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.testde_stress.lauren.testde_stress.test;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "com.testde_stress.lauren.testde_stress.test";
+  public static final String BUILD_TYPE = "debug";
+  public static final String FLAVOR = "";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "1.0";
+}
